042d72f330acb5ed1acf050a1b7ec4461dacc94a
Talisson Oliveira <talissonlux@outlook.com>
2021-11-16 03:10:31 -0300

    feat: create button to close modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,6 @@
 import Modal from "react-modal"
 import { Container } from "./styles"
+import closeImg from "../../assets/close.svg"
 
 // falar para o modal qual o elemento root da aplicação (acessibilidade)
 Modal.setAppElement('#root')
@@ -17,6 +18,14 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
             overlayClassName="react-modal-overlay"
             className="react-modal-content"
         >
+            <button
+                type="button"
+                onClick={onRequestClose}
+                className="react-modal-close"
+            >
+                <img src={closeImg} alt="Fechar modal" />
+            </button>
+
             <Container>
                 <h2>Cadastrar transação</h2>
 
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -77,4 +77,18 @@ export const GlobalStyle = createGlobalStyle`
         position: relative;
         border-radius: .24rem;
     }
+
+    .react-modal-close {
+        background: transparent;
+        position: absolute;
+        right: 1.5rem;
+        top: 1.5rem;
+        border: 0;
+
+        transition: filter .2s;
+
+        &:hover {
+            filter: brightness(.8);
+        }
+    }
 `
