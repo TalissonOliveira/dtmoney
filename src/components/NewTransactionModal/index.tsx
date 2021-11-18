a5a15a9ada8dd4f2fed7864c1870cea3d9ee6cca
Talisson Oliveira <talissonlux@outlook.com>
2021-11-18 01:53:34 -0300

    feat: add new transaction to miragejs database

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,9 +1,10 @@
-import { useState } from "react"
+import { useState, FormEvent } from "react"
 import Modal from "react-modal"
 import { Container, RadioBox, TransactionTypeContainer } from "./styles"
 import closeImg from "../../assets/close.svg"
 import incomeImg from "../../assets/income.svg"
 import outcomeImg from "../../assets/outcome.svg"
+import { api } from "../../services/api"
 
 // falar para o modal qual o elemento root da aplicação (acessibilidade)
 Modal.setAppElement('#root')
@@ -15,7 +16,23 @@ interface NewTransactionModalProps {
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
     const [transactionType, setTransactionType] = useState('deposit')
-    
+    const [title, setTitle] = useState('')
+    const [value, setValue] = useState(0)
+    const [category, setCategory] = useState('')
+
+    function handleCreateNewTransaction(event: FormEvent) {
+        event.preventDefault()
+
+        const data = {
+            title,
+            value,
+            transactionType,
+            category,
+        }
+
+        api.post('/transactions', data)
+    }
+
     return (
         <Modal
             isOpen={isOpen}
@@ -31,16 +48,20 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                 <img src={closeImg} alt="Fechar modal" />
             </button>
 
-            <Container>
+            <Container onSubmit={handleCreateNewTransaction}>
                 <h2>Cadastrar transação</h2>
 
                 <input
                     placeholder="Título"
+                    value={title}
+                    onChange={event => setTitle(event.target.value)}
                 />
 
                 <input
                     type="number"
                     placeholder="Valor"
+                    value={value}
+                    onChange={event => setValue(Number(event.target.value))}
                 />
 
                 <TransactionTypeContainer>
@@ -67,6 +88,8 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
 
                 <input
                     placeholder="Categória"
+                    value={category}
+                    onChange={event => setCategory(event.target.value)}
                 />
 
                 <button type="submit">
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
-import { createServer } from 'miragejs'
+import { createServer, Model } from 'miragejs'
 
 createServer({
+  // bando de dados interno do miragejs
+  models: {
+    // nome da tabela
+    transaction: Model
+  },
+  
   routes() {
     this.namespace = 'api'
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          category: 'Food',
-          type: 'deposit',
-          createdAt: new Date()
-        }
-      ]
+      return this.schema.all('transaction')
+    })
+
+    this.post('transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      // inserir transação no banco de dados do miragejs
+      return schema.create('transaction', data)
     })
   }
 })
