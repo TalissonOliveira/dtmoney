78a4eb0b13c47a65d81155d67eb57c6b4ef94a93
Talisson Oliveira <talissonlux@outlook.com>
2021-11-23 23:34:47 -0300

    feat: adds functionality to clear fields when closing modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -22,15 +22,21 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
     const [amount, setAmount] = useState(0)
     const [category, setCategory] = useState('')
 
-    function handleCreateNewTransaction(event: FormEvent) {
+    async function handleCreateNewTransaction(event: FormEvent) {
         event.preventDefault()
 
-        createTransaction({
+        await createTransaction({
             title,
             amount,
             type,
             category
         })
+
+        setTitle('')
+        setAmount(0)
+        setType('deposit')
+        setCategory('')
+        onRequestClose()
     }
 
     return (
