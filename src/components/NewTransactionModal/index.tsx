6b968d84e2c38184397b90b791ba13060ef53563
Talisson Oliveira <talissonlux@outlook.com>
2021-12-06 08:48:54 -0300

    feat: create a hook for transactions

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,11 +1,10 @@
-import { useState, FormEvent, useContext } from "react"
+import { useState, FormEvent } from "react"
 import Modal from "react-modal"
 import { Container, RadioBox, TransactionTypeContainer } from "./styles"
 import closeImg from "../../assets/close.svg"
 import incomeImg from "../../assets/income.svg"
 import outcomeImg from "../../assets/outcome.svg"
-import { api } from "../../services/api"
-import { TransactionsContext } from "../../TransactionsContext"
+import { useTransactions } from "../../hooks/useTransactions"
 
 // falar para o modal qual o elemento root da aplicação (acessibilidade)
 Modal.setAppElement('#root')
@@ -16,7 +15,7 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
-    const { createTransaction } = useContext(TransactionsContext)
+    const { createTransaction } = useTransactions()
     const [type, setType] = useState('deposit')
     const [title, setTitle] = useState('')
     const [amount, setAmount] = useState(0)
